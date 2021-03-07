d16120a95a58b4c6fdcf5a0965b7924695219494
Marivaldo Sena <marivaldo.sena@live.com>
2021-03-07 16:49:01 -0300

    Add and configure environment variables to application.properties file. Create PeopleController and sketch endpoint. Add Person, Phone and PhoneType entities.

---
+++ src/main/java/com/github/marivaldosena/personapi/entity/Phone.java
@@ -0,0 +1,26 @@
+package com.github.marivaldosena.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
--- src/test/java/com/github/marivaldosena/personapi/PersonapiApplicationTests.java
+++ src/test/java/com/github/marivaldosena/personapi/PersonapiApplicationTests.java
@@ -1,13 +1,7 @@
 package com.github.marivaldosena.personapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PersonapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
